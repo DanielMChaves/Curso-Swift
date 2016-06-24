91174fb771afd8312e923629b3f9c82b96ae4c48
DanielMChaves <dmelero@MacBook-Pro-de-Daniel.local>
2016-06-24 10:52:49 +0200

    Lección 1
    Declaración simple de variables y constantes

---
+++ leccion1.swift
@@ -0,0 +1,11 @@
+/* Leccion 1:  Variables, Constantes, Inferencia y Colecciones */
+
+/* 1.1 Variables y constantes */
+
+// Declaracion de variables
+var manzanas = 1
+var nombre = "Daniel"
+
+// Declaracion de constantes
+let peras = 1
+let apellido = "Melero"
